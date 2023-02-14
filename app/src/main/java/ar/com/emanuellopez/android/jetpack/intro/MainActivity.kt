af63c44bca8670d4904a83b10b947fe02641d1f0
Emanuel López <emanuel.n.lopez@gmail.com>
2023-02-14 20:04:12 -0300

    Introduction to modifiers

--- app/src/main/java/ar/com/emanuellopez/android/jetpack/intro/MainActivity.kt
+++ app/src/main/java/ar/com/emanuellopez/android/jetpack/intro/MainActivity.kt
@@ -1,28 +1,48 @@
 package ar.com.emanuellopez.android.jetpack.intro
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ar.com.emanuellopez.android.jetpack.intro.ui.theme.JetpackComposeIntroductionTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComponents()
+            JetpackComposeIntroductionTheme() {
+                MyComponents()
+            }
         }
     }
 }
 
 @Composable
 fun MyComponents() {
-    Row {
+    Row(
+        modifier = Modifier
+            .background(MaterialTheme.colors.background)
+            .padding(8.dp)
+    ) {
         MyImage()
         MyTexts()
     }
@@ -32,25 +52,41 @@ fun MyComponents() {
 fun MyImage() {
     Image(
         painterResource(id = R.drawable.ic_launcher_foreground),
-        "This is a test image"
+        "This is a test image",
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(MaterialTheme.colors.primary)
+            .size(64.dp)
     )
 }
 
 @Composable
 fun MyTexts() {
-    Column() {
-        MyText("Hello Jetpack Compose!")
-        MyText("This is another text")
+    Column(modifier = Modifier.padding(start = 8.dp)) {
+        MyText(
+            "Hello Jetpack Compose!",
+            color = MaterialTheme.colors.primary,
+            style = MaterialTheme.typography.subtitle1
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        MyText(
+            "This is another text",
+            color = MaterialTheme.colors.onBackground,
+            style = MaterialTheme.typography.subtitle2
+        )
     }
 }
 
 @Composable
-fun MyText(text: String) {
-    Text(text)
+fun MyText(text: String, color: Color, style: TextStyle) {
+    Text(text, color = color, style = style)
 }
 
 @Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewComponents() {
-    MyComponents()
+    JetpackComposeIntroductionTheme() {
+        MyComponents()
+    }
 }
